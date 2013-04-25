380f67fb35ea5e956653c9a1b42232834dfa296d
Tim van Steenbergen <tim@tieka.nl>
2013-04-25 06:53:47 +0200

    Database contact established.

--- config/autoload/global.php
+++ config/autoload/global.php
@@ -10,18 +10,18 @@
  * control, so do not include passwords or other sensitive information in this
  * file.
  */
-return array (
-		'db' => array (
-				'driver' => 'Pdo',
-				'dsn' => 'mysql:dbname=tpg;host=devel.tieka.nl',
-				'driver_options' => array (
-						PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES \'UTF8\'' 
-				) 
-		),
-
-		'service_manager' => array(
-				'factories' => array(
-						'Zend\Db\Adapter\Adapter' => 'Zend\Db\Adapter\AdapterServiceFactory'
-				)
-		)
+return array(
+    'db' => array(
+        'driver' => 'Pdo',
+        'dsn' => 'mysql:dbname=tpg;host=devel.tieka.nl',
+        'driver_options' => array(
+            PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES \'UTF8\''
+    )
+        ),
+    
+    'service_manager' => array(
+        'factories' => array(
+            'Zend\Db\Adapter\Adapter' => 'Zend\Db\Adapter\AdapterServiceFactory'
+        )
+    )
 );
--- module/Game/src/Game/Model/RoundTable.php
+++ module/Game/src/Game/Model/RoundTable.php
@@ -1,22 +1,29 @@
 <?php
 
 namespace Game\Model;
-
+ 
 use Zend\Db\Adapter\Adapter;
 use Zend\Db\TableGateway\AbstractTableGateway;
 
-class RoundTable extends AbstractTableGateway {
+class RoundTable extends AbstractTableGateway
+{
 	protected $table = 'h_rounds';
-	public function __construct(Adapter $adapter) {
-		$this->adapter = $adapter;
-	}
-	public function fetchAll() {
-		$resultSet = $this->select ( function ($select) {
-			$select->order ( 'id ASC' );
-		} );
-		$entities = array ();
-		foreach ( $resultSet as $row ) {
-			$entity = new Entity\Round ();
+			
+	public function __construct (Adapter $adapter)
+		{
+			$this->adapter = $adapter;
+		}
+	
+		public function fetchAll ()
+		{
+			$resultSet = $this->select(function  ($select)
+			{
+				$select->order('id ASC');
+			});
+			$entities = array();
+			foreach ($resultSet as $row) {
+				$entity = new Entity\Round();
+				$entity->setId($row->id);
 			$entity->setId ( $row->id );
 			$entity->setId_game ( $row->id_game );
 			$entity->setId_role ( $row->id_role );
@@ -27,53 +34,59 @@ class RoundTable extends AbstractTableGateway {
 			$entity->setIn_stock ( $row->in_stock );
 			$entity->setBacklog ( $row->backlog );
 			$entity->setOrdered ( $row->ordered );
-			$entities [] = $entity;
+				$entities[] = $entity;
+			}
+	
+			return $entities;
 		}
-		
-		return $entities;
-	}
-	public function getRound($id) {
-		$row = $this->select ( array (
-				'id' => ( int ) $id 
-		) )->current ();
-		if (! $row)
-			return false;
-		
-		$Round = new Entity\Round ( array (
-				'auto_id' => $row->auto_id,
-				'url' => $row->url,
-				'status' => $row->status,
-				'status_check' => $row->status_check,
-				'reden' => $row->reden 
-		) );
-		return Round;
-	}
-	public function saveRound(Entity\Round $Round) {
-		$data = array (
-				'note' => $Round->getNote (),
-				'created' => $Round->getCreated () 
-		);
-		
-		$id = ( int ) $Round->getId ();
-		
-		if ($id == 0) {
-			$data ['created'] = date ( "Y-m-d H:i:s" );
-			if (! $this->insert ( $data ))
+	
+		public function getRound ($id)
+		{
+			$row = $this->select(array(
+					'id' => (int) $id
+			))->current();
+			if (! $row)
 				return false;
-			return $this->getLastInsertValue ();
-		} elseif ($this->getRound ( $id )) {
-			if (! $this->update ( $data, array (
-					'id' => $id 
-			) ))
+	
+			$Round = new Entity\Round(array(
+					'auto_id' => $row->auto_id,
+					'url' => $row->url,
+					'status' => $row->status,
+					'status_check' => $row->status_check,
+					'reden' => $row->reden
+			));
+			return Round;
+		}
+	
+		public function saveRound (Entity\Round $Round)
+		{
+			$data = array(
+					'note' => $Round->getNote(),
+					'created' => $Round->getCreated()
+			);
+	
+			$id = (int) $Round->getId();
+	
+			if ($id == 0) {
+				$data['created'] = date("Y-m-d H:i:s");
+				if (! $this->insert($data))
+					return false;
+				return $this->getLastInsertValue();
+			} elseif ($this->getRound($id)) {
+				if (! $this->update($data, array(
+						'id' => $id
+				)))
+					return false;
+				return $id;
+			} else
 				return false;
-			return $id;
-		} else
-			return false;
-	}
-	public function removeRound($id) {
-		return $this->delete ( array (
-				'id' => ( int ) $id 
-		) );
-	}
+		}
+	
+		public function removeRound ($id)
+		{
+			return $this->delete(array(
+					'id' => (int) $id
+			));
+		}
 }
 
